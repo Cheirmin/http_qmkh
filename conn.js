07327da2742eda1d20c40ebc4e3dfd939f057f2f
cheirmin <1147905069@qq.com>
2018-05-08 15:26:51 +0800

    框架
    初始化框架

---
+++ conn.js
@@ -0,0 +1,32 @@
+// create the connection to database
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'mydb',
+  password:'l9yus5nn'
+});
+
+module.exports = connection;
+
+// simple query
+// connection.query(
+//   'SELECT * FROM `users` ',
+//   function(err, results, fields) {
+//
+//     for(let i=0; i<results.length;i++){
+//       console.log(results[i].username, ': ',results[i].id);
+//     }
+//     //console.log(results[0].username); // results contains rows returned by server
+//     //console.log("\n fields:\n");
+//     //console.log(fields); // fields contains extra meta data about results, if available
+//   }
+// );
+//
+//
+// connection.query(
+//   'update `users`  set `username`=? WHERE `id` = ?',['lys2', 1],
+//   function(err, results) {
+//     //console.log(results);
+//   }
+// );
