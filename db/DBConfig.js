a52172879bd2cc87344ff6c44d676f13bfcdd890
ZhouShaoxuan <37856867+ZhouShaoxuan@users.noreply.github.com>
2018-05-16 10:32:05 +0800

    同步
    123

--- db/DBConfig.js
+++ db/DBConfig.js
@@ -1,10 +1,9 @@
-module.exports =
- {
-             mysql: {
-                         host: 'localhost',
-                         user: 'root',
-                         password: 'password',
-                         database:'mydb', // 前面建的user表位于这个数据库中
-                          port: 3306
-                    }
- };
+module.exports = {
+  mysql: {
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'mydb', // 前面建的user表位于这个数据库中
+    port: 3306
+  }
+};
